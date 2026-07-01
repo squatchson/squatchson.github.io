806b3844afd4f87b3009e57f8ae80672e8e0bbd0
squatchson <mpdawson45@gmail.com>
2026-06-30 23:25:45 -0500

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,11 @@
+function toggleMenu() {
+document.querySelector('.sidebar').classList.toggle('active');
+document.querySelector('.button').classList.toggle('active');
+}
+function clog(){
+    console.log("hello world")
+}
+
+function alert1(){
+    alert("this is a test")
+}
