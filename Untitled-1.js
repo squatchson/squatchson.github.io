2c7cf247ef8400afdd0797c574e1528eb4296ee1
squatchson <mpdawson45@gmail.com>
2026-07-16 18:07:04 -0500

    Added wall of shame

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,6 +1,6 @@
 function toggleMenu() {
-document.querySelector('.sidebar').classList.toggle('active');
-document.querySelector('.button').classList.toggle('active');
+    document.querySelector('.sidebar').classList.toggle('active');
+    document.querySelector('.button').classList.toggle('active');
 }
 function clog(){
     console.log("hello world")
@@ -9,3 +9,10 @@ function clog(){
 function alert1(){
     alert("this is a test")
 }
+
+const pastDate = new Date('2026-3-5')
+const nowDate = new Date()
+const msPerDay = 24 * 60 * 60 * 1000
+const past=Math.floor((nowDate-pastDate)/msPerDay)
+console.log(past)
+document.getElementById("thatchSiteUpdate").innerHTML="<p>Thatch, its been "+past+" days, lock tf in.<br>shame</p>";
